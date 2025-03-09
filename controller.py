a277c0637dab8897fa8fda62a776a1aca33eb171
gilbertrendon <jilberlv@hotmail.com>
2025-03-08 20:43:38 -0500

    ya se crean reservas

--- controller.py
+++ controller.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from config import db  # Importamos el objeto db
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
@@ -14,20 +15,30 @@ def ejemplo():
 
 @app.route('/reservas', methods=['POST'])
 def crear_reserva():
-    # Aquí manejaremos la creación de una reserva
+    # Aquí manejamos la creación de una reserva
     data = request.get_json()
-    #  Verificamos que la información necesaria está presente
-    if not data or 'usuario' not in data or 'vehiculo' not in data or 'fecha' not in data:
-       return jsonify({'message':'Información inválida'}), 400
 
+    # Verificamos que la información necesaria esté presente
+    if not data or 'usuario' not in data or 'vehiculo' not in data:
+        return jsonify({'message': 'Información Invalida'}), 400
+    
+    #Verificamos que el usuario, vehiculo y fecha sean valores validos
+    if not isinstance(data['usuario'], str) or not isinstance(data['vehiculo'], str):
+        return jsonify({'message': 'Formato de Datos invalido'}), 400
+    
     nueva_reserva = {
-        'id': len(reservas) + 1, # Usamos un ID sencillo para este ejemplo
-        'usuario': data['usuario'],
-        'vehiculo': data['vehiculo'],
-        'fecha': data['fecha']
+        "_id" : ObjectId(),
+        'usuario_id': data['usuario'],
+        'vehiculo_id': data['vehiculo'],
+        'fecha_inicio': data['fecha_inicio'],
+        'fecha_fin': data['fecha_fin'],
+        'estado': "Reservado"
     }
-    reservas.append(nueva_reserva)
-    return jsonify({'message':'Reserva creada con éxito', 'reserva': nueva_reserva}), 201
+
+    db.reservas.insert_one(nueva_reserva)
+    # Convertimos el _id a string para la respuesta JSON
+    nueva_reserva["_id"] = str(nueva_reserva["_id"])
+    return jsonify({'message': 'Reserva creada con éxito', 'nueva_reserva': nueva_reserva}), 201
 
 @app.route('/reservas/<int:reserva_id>', methods=['DELETE'])
 def cancelar_reserva(reserva_id):
